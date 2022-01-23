42417814ff1740389e86944e09703c16852a768c
Samir-Sattarov <samirsattarov2004@gmail.com>
2022-01-23 11:59:21 +0500

    fasfas

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      initialRoute: '/welcome',
+      initialRoute: '/login',
       routes: {
         '/welcome': (context) => WelcomeScreen(),
         '/registerFirst': (ctx) => FirstRegisterScreen(),
